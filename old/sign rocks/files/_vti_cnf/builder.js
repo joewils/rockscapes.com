72c9521b2fd3584586528e43e7c8c0f4aa93de2e
Joe Wilson <joe@joecode.com>
2016-10-13 11:29:06 -0700

    iPower Backup

---
+++ old/sign rocks/files/_vti_cnf/builder.js
@@ -0,0 +1,12 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|05 Dec 2011 03:03:28 -0000
+vti_author:SR|rockscapes
+vti_modifiedby:SR|rockscapes
+vti_nexttolasttimemodified:TR|05 Dec 2011 03:03:28 -0000
+vti_timecreated:TR|06 Dec 2011 20:59:08 -0000
+vti_extenderversion:SR|5.0.2.4803
+vti_syncwith_localhost\\c\:\\users\\keoni\\documents\\my web sites\\new jr/c\:/users/keoni/documents/my web sites/new jr:TR|05 Dec 2011 03:03:28 -0000
+vti_backlinkinfo:VX|
+vti_cacheddtm:TX|16 Jan 2012 03:27:45 -0000
+vti_filesize:IR|4744
+vti_syncwith_localhost\\c\:\\users\\allan park\\documents\\my web sites\\rockscapes/c\:/users/allan park/documents/my web sites/rockscapes:TR|05 Dec 2011 03:03:28 -0000
---
+++ old/water features and ponds/files/_vti_cnf/prototype.js
@@ -0,0 +1,12 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|05 Dec 2011 03:04:50 -0000
+vti_author:SR|rockscapes
+vti_modifiedby:SR|rockscapes
+vti_nexttolasttimemodified:TR|05 Dec 2011 03:04:50 -0000
+vti_timecreated:TR|06 Dec 2011 21:00:01 -0000
+vti_extenderversion:SR|5.0.2.4803
+vti_syncwith_localhost\\c\:\\users\\keoni\\documents\\my web sites\\new jr/c\:/users/keoni/documents/my web sites/new jr:TR|05 Dec 2011 03:04:50 -0000
+vti_backlinkinfo:VX|water\\ features\\ and\\ ponds/index.html
+vti_cacheddtm:TX|16 Jan 2012 03:28:31 -0000
+vti_filesize:IR|163312
+vti_syncwith_localhost\\c\:\\users\\allan park\\documents\\my web sites\\rockscapes/c\:/users/allan park/documents/my web sites/rockscapes:TR|05 Dec 2011 03:04:50 -0000
